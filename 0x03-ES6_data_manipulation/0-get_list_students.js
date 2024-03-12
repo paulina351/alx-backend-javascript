0a9737d7b0d92bce832c89f5665a4583cb53f7ae
Paulina Udeh <udehpaulina93@gmail.com>
2024-03-12 03:00:31 +0100

    basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,18 @@
+const arrayOfObjects = [
+  {
+    // eslint-disable-next-line quote-props
+    'id': 1, 'firstName': 'Guillaume', 'location': 'San Francisco',
+  },
+  {
+    // eslint-disable-next-line quote-props, quotes
+    'id': 2, 'firstName': 'James', 'location': "Columbia",
+  },
+  {
+    // eslint-disable-next-line quote-props, quotes
+    'id': 3, 'firstName': 'Serena', 'location': 'San Francisco',
+  },
+];
+
+export default function getListStudents() {
+  return (arrayOfObjects);
+}
